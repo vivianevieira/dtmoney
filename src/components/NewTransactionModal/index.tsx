701bf40616b22cf0381ca677cfaafc3a0de44c06
Viviane Vieira <viviane.kodama@gmail.com>
2021-03-24 21:35:44 -0700

    Added buttons to the modal.

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
 import closeImg from '../../assets/close.svg';
-import { Container } from './styles';
+import { Container, TransactionTypeContainer } from './styles';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -34,6 +36,23 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
           type="number"
           placeholder="Amount"
         />
+
+        <TransactionTypeContainer>
+          <button
+            type="button"
+          >
+            <img src={incomeImg} alt="Income"/>
+            <span>Income</span>
+          </button>
+
+          <button
+            type="button"
+          >
+            <img src={outcomeImg} alt="Outcome"/>
+            <span>Outcome</span>
+          </button>
+
+        </TransactionTypeContainer>
 
         <input
           placeholder="Category"
