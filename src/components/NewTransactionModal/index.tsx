7845c046b9b3fa4e5e113fc9d301772fd753c874
Viviane Vieira <viviane.kodama@gmail.com>
2021-03-26 15:55:40 -0700

    Created a post route on the API.

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -6,6 +6,8 @@ import closeImg from '../../assets/close.svg';
 import { Container, TransactionTypeContainer, RadioBox } from './styles';
 import { FormEvent, useState } from 'react';
 
+import { api } from '../../services/api';
+
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
@@ -19,12 +21,15 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
 
   function  handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
-    console.log(
+
+    const data = {
       title,
       value,
       category,
       type
-    )
+    };
+
+    api.post('/transactions', data);
   }
 
   return (
@@ -76,8 +81,8 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
             isActive={type === 'withdraw'}
             activeColor="red"
           >
-            <img src={outcomeImg} alt="Outcome"/>
-            <span>Outcome</span>
+            <img src={outcomeImg} alt="Expense"/>
+            <span>Expense</span>
           </RadioBox>
 
         </TransactionTypeContainer>
